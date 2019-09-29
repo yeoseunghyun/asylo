574f13c2043fa9d3b496f59b282df7d0db3c93f3
Seehwan Yoo <seehwan.yoo@dankook.ac.kr>
2019-09-30 00:43:28 +0900

    application updates
    	app updates for migration

--- asylo/examples/hello_mig/hello_driver.cc
+++ asylo/examples/hello_mig/hello_driver.cc
@@ -33,7 +33,14 @@ ABSL_FLAG(std::string, enclave_path, "", "Path to enclave to load");
 ABSL_FLAG(std::string, names, "",
           "A comma-separated list of names to pass to the enclave");
 
+asylo::EnclaveConfig GetApplicationConfig();
+
 asylo::SgxClient *client;
+int g_argc;
+char **g_argv;
+void ReloadEnclave(void *base, size_t size);
+void ResumeExecution(asylo::EnclaveManager *manager);
+void Destroy(asylo::EnclaveManager *manager);
 
 // callback for SIGSNAPSHOT
 void mig_handler(int signo) {
@@ -42,12 +49,120 @@ void mig_handler(int signo) {
   if (client != NULL) {
     //Take snapshot
     asylo::Status status = client->InitiateMigration();
+	if (!status.ok()) {
+	  LOG(QFATAL) << "InitiateMigration failed";
+	}
+  }
+
+  void *base = client->base_address();
+  size_t size = client->size();
+
+  int pid;
+  pid = fork();
+  if (pid < 0) {
+	LOG(FATAL) <<"fork failed";
+  } else if (pid == 0) {
+	//child
+
+	// now, reload enclave, then restore snapshot from migration
+	ReloadEnclave(base, size);
+
+	asylo::EnclaveManager::Configure(asylo::EnclaveManagerOptions());
+	auto manager_result = asylo::EnclaveManager::Instance();
+	if (!manager_result.ok()) {
+		LOG(QFATAL) << "EnclaveManager unavailable: " << manager_result.status();
+	}
+	asylo::EnclaveManager *manager = manager_result.ValueOrDie();
+
+	ResumeExecution(manager);
+	Destroy(manager);
+	exit(0);
+	// never reach here
+	return;
+  } else if (pid > 0) {
+	//parent
   }
 }
 
+void ReloadEnclave(void *base, size_t size) {
+
+  // Part 1: Initialization
+  asylo::EnclaveManager::Configure(asylo::EnclaveManagerOptions());
+  auto manager_result = asylo::EnclaveManager::Instance();
+  if (!manager_result.ok()) {
+    LOG(QFATAL) << "EnclaveManager unavailable: " << manager_result.status();
+  }
+  asylo::EnclaveManager *manager = manager_result.ValueOrDie();
+  std::cout << "Loading " << absl::GetFlag(FLAGS_enclave_path) << std::endl;
+  asylo::SgxLoader loader(absl::GetFlag(FLAGS_enclave_path), /*debug=*/true);
+  //asylo::EnclaveConfig config = GetApplicationConfig();
+  asylo::Status status = manager->LoadEnclave("hello_enclave", loader, base, size);
+  //asylo::Status status = manager->LoadEnclave("hello_enclave", loader, config, base, size);
+  if (!status.ok()) {
+    LOG(QFATAL) << "Load " << absl::GetFlag(FLAGS_enclave_path)
+                << " failed: " << status;
+  }
+
+}
+
+void ResumeExecution(asylo::EnclaveManager *manager) {
+
+  // Part 0: Setup
+  absl::ParseCommandLine(g_argc, g_argv);
+
+  if (absl::GetFlag(FLAGS_names).empty()) {
+    LOG(QFATAL) << "Must supply a non-empty list of names with --names";
+  }
+
+  std::vector<std::string> names =
+      absl::StrSplit(absl::GetFlag(FLAGS_names), ',');
+
+
+  // Part 2: Secure execution
+  client = reinterpret_cast<asylo::SgxClient *>(manager->GetClient("hello_enclave"));
+
+  for (const auto &name : names) {
+    asylo::EnclaveInput input;
+    input.MutableExtension(hello_world::enclave_input_hello)
+        ->set_to_greet(name);
+
+    asylo::EnclaveOutput output;
+    asylo::Status status = client->EnterAndRun(input, &output);
+    if (!status.ok()) {
+      LOG(QFATAL) << "EnterAndRun failed: " << status;
+    }
+
+    if (!output.HasExtension(hello_world::enclave_output_hello)) {
+      LOG(QFATAL) << "Enclave did not assign an ID for " << name;
+    }
+
+    std::cout << "Message from enclave: "
+              << output.GetExtension(hello_world::enclave_output_hello)
+                     .greeting_message()
+              << std::endl;
+  }
+
+}
+
+void Destroy(asylo::EnclaveManager *manager) {
+
+  // Part 3: Finalization
+
+  asylo::Status status;
+  asylo::EnclaveFinal final_input;
+  status = manager->DestroyEnclave(client, final_input);
+  if (!status.ok()) {
+    LOG(QFATAL) << "Destroy " << absl::GetFlag(FLAGS_enclave_path)
+                << " failed: " << status;
+  }
+
+
+}
 
 int main(int argc, char *argv[]) {
 
+  g_argc = argc;
+  g_argv = argv;
   // signal handler for takesnapshot
   struct sigaction old_sa;
   struct sigaction new_sa;
@@ -73,7 +188,13 @@ int main(int argc, char *argv[]) {
   }
   asylo::EnclaveManager *manager = manager_result.ValueOrDie();
   std::cout << "Loading " << absl::GetFlag(FLAGS_enclave_path) << std::endl;
+
+
+  //asylo::EnclaveConfig config = GetApplicationConfig();
+  //config.set_enable_fork(true);
+
   asylo::SgxLoader loader(absl::GetFlag(FLAGS_enclave_path), /*debug=*/true);
+  //asylo::Status status = manager->LoadEnclave("hello_enclave", loader, config);
   asylo::Status status = manager->LoadEnclave("hello_enclave", loader);
   if (!status.ok()) {
     LOG(QFATAL) << "Load " << absl::GetFlag(FLAGS_enclave_path)
@@ -81,7 +202,6 @@ int main(int argc, char *argv[]) {
   }
 
   // Part 2: Secure execution
-
   client = reinterpret_cast<asylo::SgxClient *>(manager->GetClient("hello_enclave"));
 
   for (const auto &name : names) {
